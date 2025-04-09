142ce4270d40bf86bf149df571532fae86c72ab9
Ferry-200 <biyanci1@outlook.com>
2025-04-09 21:36:26 +0800

    login page

---
+++ src/jellyfin/index.ts
@@ -0,0 +1,40 @@
+import { Jellyfin } from "@jellyfin/sdk";
+import { ROUTER } from "../router";
+
+const jellyfin = new Jellyfin({
+    clientInfo: {
+        name: 'Coriander Music',
+        version: '1.0.0'
+    },
+    deviceInfo: {
+        name: navigator.userAgent,
+        id: navigator.userAgent
+    }
+})
+
+export const jellyfinApi = jellyfin
+    .createApi("http://localhost:8096/", localStorage.getItem('token') || '')
+
+jellyfinApi
+    .axiosInstance
+    .interceptors
+    .response
+    .use((response) => response, (error) => {
+        ROUTER.navigate('/login', { replace: true })
+        console.log(error)
+        return Promise.reject(error)
+    })
+
+export function authenticate(username: string, password: string) {
+    return jellyfinApi
+        .authenticateUserByName(username, password)
+        .then<boolean, boolean>(
+            (value) => {
+                localStorage.setItem('token', value.data.AccessToken || '')
+                return true
+            },
+            () => {
+                return false
+            }
+        )
+}
---
+++ src/pages/login-page.tsx
@@ -0,0 +1,69 @@
+import { styled } from "@linaria/react"
+import { TextField, TextFieldHandle } from "../component/text-field"
+import { useCallback, useRef } from "react"
+import { FilledButton } from "../component/button"
+import { authenticate } from "../jellyfin"
+import { useNavigate } from "react-router"
+import { BREAKPOINT } from "../const"
+import { ROUTE_PATH } from "../router"
+
+const PageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
+
+const PageMain = styled.main`
+  margin: auto;
+  width: 70%;
+  height: 240px;
+  padding: 8px 36px;
+  background-color: var(--md-primary-container);
+  color: var(--md-on-primary-container);
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+
+  @media screen and (min-width: ${BREAKPOINT.medium}) {
+    width: 400px;
+  }
+`
+
+const LoginTitle = styled.span`
+  font-size: 24px;
+`
+
+const LoginTextField = styled(TextField)`
+  align-self: stretch;
+`
+
+export const LoginPage = () => {
+  const usernameHandle = useRef<TextFieldHandle>(null)
+  const passwordHandle = useRef<TextFieldHandle>(null)
+  const navTo = useNavigate()
+
+  const login = useCallback(() => {
+    authenticate(
+      usernameHandle.current!.getText(),
+      passwordHandle.current!.getText()
+    ).then((result) => {
+      if (result) {
+        navTo(ROUTE_PATH.index, { replace: true })
+      }
+    })
+  }, [])
+
+  return (
+    <PageWrapper>
+      <PageMain>
+        <LoginTitle>Coriander Music</LoginTitle>
+        <LoginTextField id="username-input" labelStr='用户名' ref={usernameHandle} />
+        <LoginTextField password id="password-input" labelStr='密码' ref={passwordHandle} />
+        <FilledButton text="登陆" onClick={login} />
+      </PageMain>
+    </PageWrapper>
+  )
+}
