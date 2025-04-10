2ce2571884ba0ce25d6b793a85970af48bf83535
Ferry-200 <biyanci1@outlook.com>
2025-04-10 15:14:29 +0800

    wrap outlet with scaffold

--- src/pages/auth-guard.tsx
+++ src/pages/auth-guard.tsx
@@ -2,17 +2,22 @@ import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router";
 import { jellyfinApi } from "../jellyfin";
 import { ROUTE_PATH } from "../router";
+import { Scaffold } from "./auth-required/scaffold";
 
 export const AuthGuard = () => {
-    const navTo = useNavigate()
-    const location = useLocation()
-    useEffect(() => {
-        if (jellyfinApi.accessToken.length === 0) {
-            if (location.pathname !== ROUTE_PATH.login) {
-                navTo(ROUTE_PATH.login, { replace: true })
-            }
-        }
-    }, [])
+  const navTo = useNavigate()
+  const location = useLocation()
+  useEffect(() => {
+    if (jellyfinApi.accessToken.length === 0) {
+      if (location.pathname !== ROUTE_PATH.login) {
+        navTo(ROUTE_PATH.login, { replace: true })
+      }
+    }
+  }, [])
 
-    return <Outlet />
+  return (
+    <Scaffold>
+      <Outlet />
+    </Scaffold>
+  )
 }
