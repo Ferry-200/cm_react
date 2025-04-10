fe4d476d8789a52b9effc2c077a1de7019fa966c
Ferry-200 <biyanci1@outlook.com>
2025-04-10 15:13:44 +0800

    md3 scaffold

---
+++ src/pages/auth-required/scaffold.tsx
@@ -0,0 +1,76 @@
+import { styled } from "@linaria/react"
+import { ReactNode, useCallback, useState } from "react"
+import { StandardIconButton } from "../../component/icon-button"
+import { LucideSearch, LucideSidebar } from "lucide-react"
+import { useLocation } from "react-router"
+import { ROUTE_PATH } from "../../router"
+import { NavDrawer } from "./component/nav-drawer"
+
+type ScaffoldProp = {
+  children: ReactNode
+}
+
+const Header = styled.header`
+  height: 56px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+
+  &>span {
+    font-size: 22px;
+  }
+`
+
+function getRouteName(route: string) {
+  if (route === ROUTE_PATH.index) {
+    return '音乐'
+  } else if (route === ROUTE_PATH.artist) {
+    return '艺术家'
+  } else if (route === ROUTE_PATH.album) {
+    return '专辑'
+  } else if (route.includes('detail')) {
+    return '详情'
+  }
+
+  return 'Coriander Music'
+}
+
+const NavDrawerButton = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
+  const openDrawer = useCallback(() => {
+    setDrawerOpen(true)
+  }, [])
+
+  return (<>
+    <StandardIconButton onClick={openDrawer}>
+      <LucideSidebar />
+    </StandardIconButton>
+    <NavDrawer open={drawerOpen} onOpenChange={setDrawerOpen} />
+  </>)
+}
+
+const SearchButton = () => {
+  return (
+    <StandardIconButton onClick={() => { }}>
+      <LucideSearch />
+    </StandardIconButton>
+  )
+}
+
+export const Scaffold = ({ children }: ScaffoldProp) => {
+  const loc = useLocation()
+
+  return (
+    <div>
+      <Header>
+        <NavDrawerButton />
+        <span>{getRouteName(loc.pathname)}</span>
+        <SearchButton />
+      </Header>
+
+      {children}
+    </div>
+  )
+}
