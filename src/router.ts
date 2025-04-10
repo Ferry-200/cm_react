1320e83a42a50bea3bcd12b6022f48712867e02d
Ferry-200 <biyanci1@outlook.com>
2025-04-10 21:12:58 +0800

    router

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import { LoginPage } from "./pages/login-page";
 import { AuthGuard } from "./pages/auth-guard";
+import { MusicPage } from "./pages/auth-required/music-page";
+import { ArtistPage } from "./pages/auth-required/artist-page";
+import { ArtistDetailPage } from "./pages/auth-required/artist-detail-page";
+import { AlbumPage } from "./pages/auth-required/album-page";
+import { AlbumDetailPage } from "./pages/auth-required/album-detail-page";
 
 export const ROUTE_PATH = {
     login: '/login',
@@ -21,23 +26,23 @@ export const ROUTER = createBrowserRouter([
         children: [
             {
                 index: true,
-                // Component: 
+                Component: MusicPage
             },
             {
                 path: ROUTE_PATH.artist,
-                // Component: 
+                Component: ArtistPage
             },
             {
                 path: ROUTE_PATH.artistDetail,
-                // Component: 
+                Component: ArtistDetailPage
             },
             {
                 path: ROUTE_PATH.album,
-                // Component: 
+                Component: AlbumPage
             },
             {
                 path: ROUTE_PATH.albumDetail,
-                // Component: 
+                Component: AlbumDetailPage
             }
         ]
     }
