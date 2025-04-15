d68536f51eba43f73d071a2058fd75fbe99d5148
Ferry-200 <biyanci1@outlook.com>
2025-04-15 23:04:21 +0800

    album page

--- src/pages/auth-required/album-page.tsx
+++ src/pages/auth-required/album-page.tsx
@@ -1,5 +1,160 @@
-export const AlbumPage = () => {
+import { styled } from "@linaria/react"
+import { useAlbums } from "./hook/use-albums"
+import { AlbumTile } from "./component/album-tile"
+import { PagingArea } from "./component/paging-area"
+import { StandardIconButton } from "../../component/icon-button"
+import { LucideSortAsc, LucideSortDesc } from "lucide-react"
+/** 使用最后一个没有 display: table; 的版本，保证兼容性和界面效果 */
+import { ScrollArea, ScrollAreaViewport, ScrollAreaScrollbar, ScrollAreaThumb } from "@radix-ui/react-scroll-area"
+import { SortOrder } from "@jellyfin/sdk/lib/generated-client/models"
+import { MenuIconButton } from "../../component/menu-icon-button"
+import { DropdownMenu } from "radix-ui"
+import { RadioGroup } from "../../component/radio-group"
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const PageHeader = styled.div`
+  flex-shrink: 0;
+  height: 48px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 8px;
+`
+
+const PageHeaderActions = styled.div`
+  margin-left: auto;
+  display: flex;
+  gap: 8px;
+`
+
+const ScrollViewRoot = styled(ScrollArea)`
+  width: 100%;
+  flex-grow: 1;
+  min-height: 0;
+`
+
+const ScrollViewMain = styled(ScrollAreaViewport)`
+  width: 100%;
+  height: 100%;
+`
+
+const ScrollViewScrollbar = styled(ScrollAreaScrollbar)`
+	/* ensures no selection */
+	user-select: none;
+	/* disable browser handling of all panning and zooming gestures on touch devices */
+	touch-action: none;
+  width: 8px;
+  padding: 8px 0;
+`
+
+const ScrollViewThumb = styled(ScrollAreaThumb)`
+	background-color: var(--md-outline);
+  width: 8px;
+	border-radius: 4px;
+`
+
+const ScrollViewPagingArea = styled(PagingArea)`
+  flex-shrink: 0;
+`
+
+const GridView = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(136px, 1fr));
+  gap: 8px;
+`
+
+type SortOrderToggleBtnProp = {
+  currOrder: SortOrder,
+  onOrderSelected: (order: SortOrder) => void
+}
+
+const SortOrderToggleBtn = ({ currOrder, onOrderSelected }: SortOrderToggleBtnProp) => {
   return (
-    <span>Album page</span>
+    <StandardIconButton
+      onClick={() => {
+        onOrderSelected(
+          currOrder === SortOrder.Ascending
+            ? SortOrder.Descending
+            : SortOrder.Ascending
+        )
+      }}
+    >
+      {
+        currOrder === SortOrder.Ascending
+          ? <LucideSortAsc />
+          : <LucideSortDesc />
+      }
+    </StandardIconButton>
+  )
+}
+
+const MenuLabel = styled(DropdownMenu.Label)`
+  margin: 4px 0 4px 12px;
+  color: var(--on-surface-variant);
+  font-weight: bold;
+`
+
+export const AlbumPage = () => {
+  const [state, result, dispatch] = useAlbums()
+  const currPage = (state.offset / state.size) + 1
+  return (
+    <Wrapper>
+      <PageHeader>
+        {result.data && <span>{result.data.TotalRecordCount!} 张专辑</span>}
+        <PageHeaderActions>
+          <SortOrderToggleBtn
+            currOrder={state.sortOrder}
+            onOrderSelected={(order) => {
+              dispatch({ type: 'setSortOrder', sortOrder: order })
+            }}
+          />
+          <MenuIconButton>
+            <MenuLabel>数量</MenuLabel>
+            <RadioGroup
+              curr={state.size.toString()}
+              onValueChange={(curr) => {
+                dispatch({ type: 'setSize', size: Number.parseInt(curr) })
+              }}
+              items={[
+                { value: '25', display: '25' },
+                { value: '50', display: '50' },
+                { value: '75', display: '75' },
+                { value: '100', display: '100' }
+              ]}
+            />
+          </MenuIconButton>
+        </PageHeaderActions>
+      </PageHeader>
+
+      <ScrollViewRoot type='scroll'>
+        {
+          result.data &&
+          <ScrollViewMain>
+            <GridView>{
+              result.data.Items!.map(
+                (item) => <AlbumTile key={item.Id} album={item} />
+              )
+            }</GridView>
+            <ScrollViewPagingArea
+              curr={currPage}
+              count={Math.ceil(result.data.TotalRecordCount! / state.size)}
+              onPaging={(page) => dispatch({
+                type: 'setOffset',
+                offset: (page - 1) * state.size
+              })}
+            />
+          </ScrollViewMain>
+        }
+        <ScrollViewScrollbar orientation="vertical">
+          <ScrollViewThumb />
+        </ScrollViewScrollbar>
+      </ScrollViewRoot>
+    </Wrapper>
   )
 }
---
+++ src/pages/auth-required/component/album-tile.tsx
@@ -0,0 +1,70 @@
+import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models"
+import { styled } from "@linaria/react"
+import { getImageStreamUrl } from "../../../jellyfin/streaming"
+import { Avatar } from "radix-ui"
+import { LucideImageOff } from "lucide-react"
+
+type AlbumTileProp = {
+  album: BaseItemDto
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  border-radius: 8px;
+  position: relative;
+  cursor: pointer;
+
+  span {
+    text-align: center;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    border-radius: inherit;
+    pointer-events: none;
+    transition: background-color 150ms;
+  }
+
+  &:hover::before {
+    background-color: var(--md-surface-hover);
+  }
+
+  &:active::before {
+    background-color: var(--md-surface-active);
+  }
+`
+
+const AlbumImg = styled(Avatar.Image)`
+  width: 112px;
+  height: 112px;
+  border-radius: 8px;
+`
+
+const AlbumImgFallback = styled(Avatar.Fallback)`
+  width: 112px;
+  height: 112px;
+`
+
+export const AlbumTile = ({ album }: AlbumTileProp) => {
+  return (
+    <Wrapper>
+      <Avatar.Root>
+        <AlbumImg src={getImageStreamUrl(album.Id!, 96)} />
+        <AlbumImgFallback asChild>
+          <LucideImageOff strokeWidth={0.5}  />
+        </AlbumImgFallback>
+      </Avatar.Root>
+      <span>{album.Name}</span>
+    </Wrapper>
+  )
+}
