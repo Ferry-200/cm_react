27441bae79d672f600155f278856eb515df9a5fc
Ferry-200 <biyanci1@outlook.com>
2025-04-15 22:53:00 +0800

    Artist page

---
+++ src/pages/auth-required/component/artist-tile.tsx
@@ -0,0 +1,70 @@
+import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models"
+import { styled } from "@linaria/react"
+import { getImageStreamUrl } from "../../../jellyfin/streaming"
+import { Avatar } from "radix-ui"
+import { LucideImageOff } from "lucide-react"
+
+type ArtistTileProp = {
+  artist: BaseItemDto
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  border-radius: 8px;
+  position: relative;
+  cursor: pointer;
+
+  span {
+    text-align: center;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    border-radius: inherit;
+    pointer-events: none;
+    transition: background-color 150ms;
+  }
+
+  &:hover::before {
+    background-color: var(--md-surface-hover);
+  }
+
+  &:active::before {
+    background-color: var(--md-surface-active);
+  }
+`
+
+const ArtistImg = styled(Avatar.Image)`
+  width: 112px;
+  height: 112px;
+  border-radius: 8px;
+`
+
+const ArtistImgFallback = styled(Avatar.Fallback)`
+  width: 112px;
+  height: 112px;
+`
+
+export const ArtistTile = ({ artist }: ArtistTileProp) => {
+  return (
+    <Wrapper>
+      <Avatar.Root>
+        <ArtistImg src={getImageStreamUrl(artist.Id!, 96)} />
+        <ArtistImgFallback asChild>
+          <LucideImageOff strokeWidth={0.5}  />
+        </ArtistImgFallback>
+      </Avatar.Root>
+      <span>{artist.Name}</span>
+    </Wrapper>
+  )
+}
