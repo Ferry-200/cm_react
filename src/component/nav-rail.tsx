b0174105e9d19e5badcb368e18997c2be1a8879a
Ferry-200 <biyanci1@outlook.com>
2025-04-19 22:41:27 +0800

    new component: MediumNavRail

--- src/component/nav-rail.tsx
+++ src/component/nav-rail.tsx
@@ -1,19 +1,115 @@
 import { styled } from "@linaria/react"
 import { BREAKPOINT } from "../utils"
+import { Link, useLocation } from "react-router"
+import { ReactNode } from "react"
+import { ROUTE_PATH } from "../router"
+import { LucideDiscAlbum, LucideMusic, LucideUsers } from "lucide-react"
 
 const Wrapper = styled.nav`
   width: 80px;
   height: 100vh;
   background-color: var(--md-surface-container);
   display: none;
+  padding: 12px;
 
   @media screen and (min-width: ${BREAKPOINT.medium}) {
     display: block;
   }
 `
 
+const DesLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  text-decoration: none;
+  color: var(--md-on-surface);
+  margin-bottom: 12px;
+`
+
+type NavRailDesProp = {
+  to: string,
+  icon: ReactNode,
+  text: string
+}
+
+const DesIconWrapper = styled.div`
+  height: 32px;
+  width: 56px;
+  border-radius: 16px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+  }
+
+  &:hover::before {
+    background-color: var(--md-surface-hover);
+  }
+
+  &:active::before {
+    background-color: var(--md-surface-active);
+  }
+
+  &.curr {
+    background-color: var(--md-secondary-container);
+    color: var(--md-on-secondary-container);
+
+    &:hover::before {
+      background-color: var(--md-secondary-container-hover);
+    }
+
+    &:active::before {
+      background-color: var(--md-secondary-container-active);
+    }
+
+    &+span {
+      font-weight: bold;
+    }
+  }
+`
+
+const NavRailDes = ({ to, icon, text }: NavRailDesProp) => {
+  const loc = useLocation()
+
+  return (
+    <DesLink to={to}>
+      <DesIconWrapper className={loc.pathname === to ? 'curr' : undefined}>
+        {icon}
+      </DesIconWrapper>
+      <span>{text}</span>
+    </DesLink>
+  )
+}
+
 export const NavRail = () => {
   return (
-    <Wrapper />
+    <Wrapper>
+      <NavRailDes
+        to={ROUTE_PATH.index}
+        icon={<LucideMusic />}
+        text="音乐"
+      />
+      <NavRailDes
+        to={ROUTE_PATH.artist}
+        icon={<LucideUsers />}
+        text="艺术家"
+      />
+      <NavRailDes
+        to={ROUTE_PATH.album}
+        icon={<LucideDiscAlbum />}
+        text="专辑"
+      />
+    </Wrapper>
   )
 }
