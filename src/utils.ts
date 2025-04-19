070cfb545f35b147d1df1a9ec999f1f5dbdda344
Ferry-200 <biyanci1@outlook.com>
2025-04-19 23:03:24 +0800

    新增检测其他屏幕大小的 hook

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,30 @@
 import { CSSProperties } from "react"
 import { useState, useEffect } from "react"
 
+export type Stylable = {
+    className?: string,
+    style?: CSSProperties,
+}
+
 export const BREAKPOINT = {
     medium: '600px',
     large: '840px',
     extraLarge: '1200px'
 }
 
-const MEDIUM_MEDIA_MATCHER = `(min-width: ${BREAKPOINT.medium})`
+const MEDIA_MATCHER = {
+    medium: `(min-width: ${BREAKPOINT.medium})`,
+    large: `(min-width: ${BREAKPOINT.large})`,
+    extraLarge: `(min-width: ${BREAKPOINT.extraLarge})`
+} as const
 
-export type Stylable = {
-    className?: string,
-    style?: CSSProperties,
-}
+type MEDIA_MATCHER = (typeof MEDIA_MATCHER)[keyof typeof MEDIA_MATCHER]
 
-export const useIsMediumScreen = () => {
+function useIsScreenType(matcher: MEDIA_MATCHER) {
     const [matches, setMatches] = useState(false)
 
     useEffect(() => {
-        const mediaQueryList = window.matchMedia(MEDIUM_MEDIA_MATCHER)
+        const mediaQueryList = window.matchMedia(matcher)
 
         const handleChange = (e: MediaQueryListEvent) => {
             setMatches(e.matches)
@@ -30,7 +36,13 @@ export const useIsMediumScreen = () => {
         return () => {
             mediaQueryList.removeEventListener("change", handleChange)
         }
-    }, [])
+    }, [matcher])
 
     return matches
 }
+
+export const useIsMediumScreen = () => useIsScreenType(MEDIA_MATCHER.medium)
+
+export const useIsLargeScreen = () => useIsScreenType(MEDIA_MATCHER.large)
+
+export const useIsExtraLargeScreen = () => useIsScreenType(MEDIA_MATCHER.extraLarge)
