05b3d3ca7d3fa2d82d9fb35ef29c2b2adc7bf7d1
Ferry-200 <biyanci1@outlook.com>
2025-05-21 09:25:52 +0800

    响应式分页区域

--- src/pages/auth-required/component/paging-area.tsx
+++ src/pages/auth-required/component/paging-area.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@linaria/react"
-import { LucideChevronLeft, LucideChevronRight } from "lucide-react"
+import { LucideChevronLeft, LucideChevronRight, LucideMoreHorizontal } from "lucide-react"
 import { Stylable } from "../../../utils"
 
 type PagingAreaProp = Stylable & {
@@ -65,18 +65,64 @@ const PagingBtn = styled.button`
   }
 `
 
+/**
+ * |<-| 1 |...| 4 |...| 7 |->|
+ * 始终显示 <-、第一页、最后一页、-> 和当前页
+ * 第一页和当前页之间的页数大于1时，折叠；否则直接显示
+ * 最后一页和当前页之间的页数大于1时，折叠；否则直接显示
+ */
 export const PagingArea = ({ className, style, curr, count, onPaging }: PagingAreaProp) => {
+  const buildPagingBtn = (p: number) => (
+    <PagingBtn
+      key={p}
+      type="button"
+      className={curr === p ? 'primary' : undefined}
+      onClick={() => onPaging(p)}
+    >{p + 1}</PagingBtn>
+  )
+
   const pagingBtns = []
-  for (let i = 0; i < count; i++) {
+
+  // 第一页
+  pagingBtns.push(buildPagingBtn(0))
+
+  // 第一页和当前页之间的页数
+  const btnCountBetweenFirstCurr = curr - 1
+  if (btnCountBetweenFirstCurr > 0) {
     pagingBtns.push(
-      <PagingBtn
-        key={i}
-        type="button"
-        className={curr === i ? 'primary' : undefined}
-        onClick={() => onPaging(i)}
-      >{i + 1}</PagingBtn>
+      btnCountBetweenFirstCurr > 1
+        ? (
+          <PagingBtn
+            key='left-ellipsis'
+            type="button"
+          ><LucideMoreHorizontal /></PagingBtn>
+        )
+        : buildPagingBtn(1)
     )
   }
+
+  // curr 在中间的情况
+  if (curr >= 1 && curr <= count - 2) {
+    pagingBtns.push(buildPagingBtn(curr))
+  }
+
+  // 最后一页和当前页之间的页数
+  const btnCountBetweenCurrLast = count - curr - 2
+  if (btnCountBetweenCurrLast > 0) {
+    pagingBtns.push(
+      btnCountBetweenCurrLast > 1
+        ? (
+          <PagingBtn
+            key='right-ellipsis'
+            type="button"
+          ><LucideMoreHorizontal /></PagingBtn>
+        )
+        : buildPagingBtn(count - 2)
+    )
+  }
+
+  // 最后一页
+  pagingBtns.push(buildPagingBtn(count - 1))
 
   const hasPrev = curr > 0
   const hasNext = curr < count - 1
