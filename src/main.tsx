df8169873d59aee65eedcd07498a40e8a7ed0d32
Ferry-200 <biyanci1@outlook.com>
2025-07-19 15:05:03 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import { createAppRouter } from './router'
 import { PlayerContext } from './player/context'
 import { createPlayer } from './player'
 
-import * as Sentry from "@sentry/react";
+import { init as initSentry } from "@sentry/react";
 
-Sentry.init({
+initSentry({
   dsn: "https://389968c86d02060f7432a2db81adefc1@o4509693787176960.ingest.de.sentry.io/4509693789012048",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
